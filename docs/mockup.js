60b79a1f0d3fc1c35a0d27d2182d4f217c639856
Francois Laberge <francoislaberge@Jibos-MacBook-Pro-2.local>
2017-09-24 13:53:50 -0400

    Added demo of streaming dewarped paper

---
+++ docs/mockup.js
@@ -0,0 +1,106 @@
+function mockup(options){
+    // Container is where the template image is inserted as well as 
+    // the element to transform
+    if(!options.container){
+        throw new Error('options.container is required');
+    }
+
+    // Add the target element into the container
+    options.container.appendChild(options.element);
+    options.element.style.transformOrigin = "0 0";
+
+    
+    // Create it's template image, insert it after so it's a mask
+    // in front of the warped element
+    const image = document.createElement('img');
+    image.src = options.template;
+    options.container.appendChild(image);
+
+    //578, 293
+
+    options.element.style.position = "absolute";
+    image.style.position = "absolute";
+    //image.style.opacity = '0.5';
+
+    // Do the actual warping
+    transform2d(options.element,
+        options.corners[0], options.corners[1], options.corners[2], options.corners[3],
+        options.corners[4], options.corners[5], options.corners[6], options.corners[7] );
+}
+
+var shiftX = 2;
+var shiftY = 0;
+
+function adj(m) { // Compute the adjugate of m
+return [
+m[4]*m[8]-m[5]*m[7], m[2]*m[7]-m[1]*m[8], m[1]*m[5]-m[2]*m[4],
+m[5]*m[6]-m[3]*m[8], m[0]*m[8]-m[2]*m[6], m[2]*m[3]-m[0]*m[5],
+m[3]*m[7]-m[4]*m[6], m[1]*m[6]-m[0]*m[7], m[0]*m[4]-m[1]*m[3]
+];
+}
+function multmm(a, b) { // multiply two matrices
+var c = Array(9);
+for (var i = 0; i != 3; ++i) {
+for (var j = 0; j != 3; ++j) {
+  var cij = 0;
+  for (var k = 0; k != 3; ++k) {
+    cij += a[3*i + k]*b[3*k + j];
+  }
+  c[3*i + j] = cij;
+}
+}
+return c;
+}
+function multmv(m, v) { // multiply matrix and vector
+return [
+m[0]*v[0] + m[1]*v[1] + m[2]*v[2],
+m[3]*v[0] + m[4]*v[1] + m[5]*v[2],
+m[6]*v[0] + m[7]*v[1] + m[8]*v[2]
+];
+}
+function pdbg(m, v) {
+var r = multmv(m, v);
+return r + " (" + r[0]/r[2] + ", " + r[1]/r[2] + ")";
+}
+function basisToPoints(x1, y1, x2, y2, x3, y3, x4, y4) {
+var m = [
+x1, x2, x3,
+y1, y2, y3,
+ 1,  1,  1
+];
+var v = multmv(adj(m), [x4, y4, 1]);
+return multmm(m, [
+v[0], 0, 0,
+0, v[1], 0,
+0, 0, v[2]
+]);
+}
+function general2DProjection(
+x1s, y1s, x1d, y1d,
+x2s, y2s, x2d, y2d,
+x3s, y3s, x3d, y3d,
+x4s, y4s, x4d, y4d
+) {
+var s = basisToPoints(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s);
+var d = basisToPoints(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d);
+return multmm(d, adj(s));
+}
+function project(m, x, y) {
+var v = multmv(m, [x, y, 1]);
+return [v[0]/v[2], v[1]/v[2]];
+}
+function transform2d(elt, x1, y1, x2, y2, x3, y3, x4, y4) {
+var w = elt.offsetWidth, h = elt.offsetHeight;
+var t = general2DProjection
+(0, 0, x1, y1, w, 0, x2, y2, 0, h, x3, y3, w, h, x4, y4);
+for(i = 0; i != 9; ++i) t[i] = t[i]/t[8];
+t = [t[0], t[3], 0, t[6],
+   t[1], t[4], 0, t[7],
+   0   , 0   , 1, 0   ,
+   t[2], t[5], 0, t[8]];
+t = "matrix3d(" + t.join(", ") + ")";
+elt.style["-webkit-transform"] = t;
+elt.style["-moz-transform"] = t;
+elt.style["-o-transform"] = t;
+elt.style.transform = t;
+}
